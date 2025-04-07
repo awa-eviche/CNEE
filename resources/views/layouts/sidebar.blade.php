a96130c22a1d80f5fdf9547e8074d251fd161090
Awa <hadiatoudiawdiop@gmail.com>
2025-04-07 09:40:36 +0000

    today

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="sidebar" data-background-color="dark">
     <div class="sidebar-logo">
         <div class="logo-header" data-background-color="dark">
-            <a href="index.html" class="logo">
-                <img src="{{ asset('assets/img/kaiadmin/logo_light.svg') }}" alt="navbar brand" class="navbar-brand" height="20"/>
+            <a href="/" class="logo">
+                <img src="{{ asset('assets/img/kaiadmin/logocnee.png') }}" alt="navbar brand" class="navbar-brand" height="20"/>
             </a>
             <div class="nav-toggle">
                 <button class="btn btn-toggle toggle-sidebar">
